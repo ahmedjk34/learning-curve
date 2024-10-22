453ca2def2c350c49a6bc2004d23c63604f1bef2
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-22 11:20:25 +0300

    Added Course Type (with sub-types being Review and Content)

---
+++ src/Types.ts
@@ -0,0 +1,40 @@
+import { Schema, Document } from "mongoose";
+
+// Define the review interface
+interface Review extends Document {
+  author: Schema.Types.ObjectId; // Reference to a User model
+  body: string;
+  rating: number;
+  createdAt: Date;
+}
+
+interface Content extends Document {
+  title: string;
+  contentType: ["Video", "Quiz"];
+  contentUrl: string;
+  duration: number;
+  order: number;
+}
+
+// Define the course interface
+export interface Course extends Document {
+  title: string;
+  description: string;
+  enrolledStudents: number;
+  learningGoals: string[];
+  prerequisites: string[];
+  reviews: Review[]; // Array of review objects
+  episodes: Content[]; // Array of episode objects
+  coverImg: string;
+  difficulty: string;
+  duration: number; // in hours
+  category:
+    | "Programming"
+    | "Mathematics"
+    | "Data Science"
+    | "Physics"
+    | "Artificial Intelligence";
+  price: number;
+  discount: number;
+  createdAt: Date;
+}
