96f96c2e630c5f4a95c849a43411f29227bcf6b6
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-24 14:41:37 +0300

    Changed the style & highlighted text in the hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,17 +17,19 @@ function Hero() {
     <div className={styles.hero}>
       <div className={styles.heroInfo}>
         <h1>
-          Master New Skills at Your Own Pace - Choose from our unique collection
-          of courses
+          Master New Skills at <span>Your Own Pace</span> - Choose from our
+          <span> unique collection</span> of courses
         </h1>
         <p>
-          Discover a world of learning on Learning Curve, where you can explore
-          a wide range of courses designed to help you grow. Whether you're
-          advancing your career or pursuing personal passions, our flexible
-          learning paths and expert-led resources will guide you to success.
-          Start your learning journey today and achieve your goals, one course
-          at a time.
+          Discover a world of learning on <span>Learning Curve</span>, where you
+          can explore a <span>wide range of courses</span> designed to help you
+          grow. Whether you're <span>advancing your career</span> or{" "}
+          <span>pursuing personal passions</span>, our flexible learning paths
+          and expert-led resources will guide you to success. Start your{" "}
+          <span>learning journey</span> today and{" "}
+          <span>achieve your goals</span>, one course at a time.
         </p>
+
         <div className={styles.heroActionsHolder}>
           <Link href={"/"}>Explore Courses</Link>
           <Link href={"/"}>Start Learning</Link>
