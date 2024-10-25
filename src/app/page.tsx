0174c23db03dd44f538de26fac88355a050c351a
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2024-10-25 13:50:17 +0300

    Changed the featured courses section to be a slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,42 +51,44 @@ function FeaturedCourses({ courses }: { courses: Course[] }) {
   return (
     <div className={styles.featuredCourses}>
       <h1>Featured Courses</h1>
-      <div className={styles.coursesHolder}>
-        {courses.map((course) => (
-          <div className={styles.course} key={v4()}>
-            <div className={styles.imageHolder}>
-              <img
-                src={course.coverImg}
-                alt={course.title}
-                className={styles.courseImage}
-              ></img>
-              <span>{course.price}$</span>
-            </div>
-            <div className={styles.courseInfo}>
-              <h2 className={styles.courseTitle}>{course.title}</h2>
-              <div className={styles.subInfoHolder}>
-                <h4>
-                  <span>Duration: </span> {course.duration.toFixed(1)} Hours
-                </h4>
-                <h4>
-                  <span>Enrolled Students: </span>
-                  {course.enrolledStudents}
-                </h4>
+      <div className={styles.coursesSlider}>
+        <div className={styles.coursesSlide}>
+          {courses.concat(courses).map((course, index) => (
+            <div className={styles.course} key={v4()}>
+              <div className={styles.imageHolder}>
+                <img
+                  src={course.coverImg}
+                  alt={course.title}
+                  className={styles.courseImage}
+                />
+                <span>{course.price.toFixed(2)}$</span>
+              </div>
+              <div className={styles.courseInfo}>
+                <h2 className={styles.courseTitle}>{course.title}</h2>
+                <div className={styles.subInfoHolder}>
+                  <h4>
+                    <span>Duration: </span> {course.duration.toFixed(1)} Hours
+                  </h4>
+                  <h4>
+                    <span>Enrolled Students: </span>
+                    {course.enrolledStudents}
+                  </h4>
+                </div>
+              </div>
+              <div className={styles.courseDescription}>
+                <p>{course.description}</p>
+              </div>
+              <div className={styles.courseActions}>
+                <button>Buy Now</button>
+                <Rating
+                  initialValue={course.rating}
+                  readonly={true}
+                  allowFraction={true}
+                />
               </div>
             </div>
-            <div className={styles.courseDescription}>
-              <p>{course.description}</p>
-            </div>
-            <div className={styles.courseActions}>
-              <button>Buy Now</button>
-              <Rating
-                initialValue={course.rating}
-                readonly={true}
-                allowFraction={true}
-              />
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
